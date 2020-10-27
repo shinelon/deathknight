533714c44147905b7c6041c317944e8354b7cb06
shinelon <syq_sunshine@163.com>
2020-10-27 18:26:33 +0800

    add SearchScrollRequest

--- src/test/java/com/shinelon/deathknight/es/test/EsSearchOptTest.java
+++ src/test/java/com/shinelon/deathknight/es/test/EsSearchOptTest.java
@@ -5,10 +5,12 @@ import java.util.concurrent.TimeUnit;
 
 import org.elasticsearch.action.search.SearchRequest;
 import org.elasticsearch.action.search.SearchResponse;
+import org.elasticsearch.action.search.SearchScrollRequest;
 import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.client.RestHighLevelClient;
 import org.elasticsearch.common.unit.TimeValue;
 import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.search.Scroll;
 import org.elasticsearch.search.SearchHit;
 import org.elasticsearch.search.SearchHits;
 import org.elasticsearch.search.builder.SearchSourceBuilder;
@@ -29,6 +31,8 @@ public class EsSearchOptTest extends DeathknightApplicationTests {
   @Test
   public void searchTest() {
     SearchRequest searchRequest = new SearchRequest("posts");
+    Scroll scroll = new Scroll(TimeValue.timeValueMinutes(1));
+    searchRequest.scroll(scroll);
     SearchSourceBuilder sourceBuilder = new SearchSourceBuilder();
     //    sourceBuilder.query(QueryBuilders.termQuery("user", "kimchy"));
     sourceBuilder.query(QueryBuilders.matchAllQuery());
@@ -40,10 +44,16 @@ public class EsSearchOptTest extends DeathknightApplicationTests {
       SearchResponse searchResponse =
           restHighLevelClient.search(searchRequest, RequestOptions.DEFAULT);
       SearchHits hits = searchResponse.getHits();
+      String scrollId = searchResponse.getScrollId();
       SearchHit[] hits2 = hits.getHits();
       for (SearchHit searchHit : hits2) {
         logger.info("hit:{}", searchHit);
       }
+      SearchScrollRequest scrollRequest = new SearchScrollRequest(scrollId);
+      scrollRequest.scroll(TimeValue.timeValueSeconds(30));
+      SearchResponse searchScrollResponse =
+          restHighLevelClient.scroll(scrollRequest, RequestOptions.DEFAULT);
+      logger.info("searchScrollResponse:{}", searchScrollResponse);
     } catch (IOException e) {
       logger.error(e.getMessage(), e);
     }
