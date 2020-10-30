ea6bc5b65c458eb62ad5e2677dd2bbf79afcade4
sunyaoqiang <sunyaoqiang@dankegongyu.com>
2020-10-30 15:12:44 +0800

    add indexcontroller

---
+++ src/main/java/com/shinelon/deathknight/web/IndexController.java
@@ -0,0 +1,16 @@
+package com.shinelon.deathknight.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/")
+public class IndexController {
+
+  @RequestMapping("/")
+  @ResponseBody
+  public String index() {
+    return "hello deathknight!";
+  }
+}
